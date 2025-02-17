478414d5a87a372d6fc4a5a50715f0375699d2d8
Ahmed-Elmoslmany <ahmedelmoslmany74@gmail.com>
2025-02-17 16:13:40 +0200

    chore: change handle candidate report method from GET to POST request

--- routes.py
+++ routes.py
@@ -13,6 +13,6 @@ def get_candidate(id):
 def create_candidate():
     return candidates.CandidateController.create_candidates()
 
-@app.route('/candidates')
+@app.route('/candidates', methods =['POST'])
 def handle_candidates_report():
     return candidates.CandidateController.handle_candidates_report()
